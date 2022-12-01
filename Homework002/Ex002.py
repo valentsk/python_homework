15b8d2800fb767f2713fad2fa5710e1ea0796562
valentsk <skredovavalenti@mail.ru>
2022-12-01 23:06:23 +0300

    Resolve ex002, 003

--- Homework002/Ex002.py
+++ Homework002/Ex002.py
@@ -3,3 +3,12 @@
 # Пример:
 #
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+number = int(input('Введите число: '))
+a = 1
+list_result = []
+for i in range(0, number):
+    a *= (i + 1)
+    list_result.append(a)
+print(list_result)
--- Homework002/Ex003.py
+++ Homework002/Ex003.py
@@ -1,5 +1,14 @@
 # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 #
-# Пример:
-#
-# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+
+number = int(input('Введите число: '))
+a = 1
+sum = 0
+list_result = []
+for i in range(1, number + 1):
+    a = round(((1 + 1 / i) ** i) , 2)
+    sum += a
+    list_result.append(a)
+print(list_result)
+print(f'Сумма элементов последовательности {sum}')
