a5341ee47640368bba2b9a835314a611379d8f3a
valentsk <skredovavalenti@mail.ru>
2022-12-03 11:27:19 +0300

    Resolve ex005

--- Homework002/Ex005.py
+++ Homework002/Ex005.py
@@ -1,1 +1,14 @@
 # Реализуйте алгоритм перемешивания списка.
+
+import random
+
+test_string = 'Реализуйте алгоритм перемешивания списка. Задайте список из N элементов, заполненных числами из промежутка [-N, N].'
+list_strings = test_string.split()
+print(list_strings)
+for i in range(len(list_strings)):
+    temp = list_strings[i]
+    random_position = random.randint(0,len(list_strings)-1)
+    list_strings[i] = list_strings[random_position]
+    list_strings[random_position] = temp
+print(list_strings)
+
