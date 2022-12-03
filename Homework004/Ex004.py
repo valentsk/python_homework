7d2144267404a6ef62e007310b72438a5d54232c
valentsk <skredovavalenti@mail.ru>
2022-12-03 16:32:50 +0300

    Resolve ex004

--- Homework004/Ex004.py
+++ Homework004/Ex004.py
@@ -4,3 +4,25 @@
 # Пример:
 #
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+stepen = random.randint(0, 101)
+list_koef_stepen_x = []
+list_koef_b =[]
+list_result = []
+
+for i in range(stepen):
+    list_koef_stepen_x.append(i)
+    list_koef_b.append(random.randint(0, 20))
+
+with open('text.txt', 'w') as file_data:
+    for i in range(len(list_koef_stepen_x) - 1, -1, -1):
+        if list_koef_b[i] != 0:
+            if i == 1:
+                file_data.writelines(f'{list_koef_b[i]} * x + ')
+            elif i == 0:
+                file_data.writelines(f'{list_koef_b[i]} ')
+            else:
+                file_data.writelines(f'{list_koef_b[i]} * x^{list_koef_stepen_x[i]} + ')
+    file_data.writelines(' = 0')
