057be3a4093ad17187fa518ec8daa6381c923c0d
valentsk <skredovavalenti@mail.ru>
2022-12-03 12:48:51 +0300

    Add resolve 003

--- Homework003/Ex003.py
+++ Homework003/Ex003.py
@@ -3,3 +3,25 @@
 # Пример:
 #
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+
+number = int(input('Введите число: '))
+list_numbers = []
+for i in range(number):
+    list_numbers.append(round(random.randint(0, 100) / random.randint(0, 100), 2))
+print(list_numbers)
+
+list_fractions = []
+for i in range(number):
+    list_fractions.append((round(list_numbers[i] % 1 * 100)))
+print(list_fractions)
+index_min = list_fractions[0]
+index_max = list_fractions[0]
+
+for i in range(number):
+    if list_fractions[i] < list_fractions[index_min]:
+        index_min = i
+    if list_fractions[i] > list_fractions[index_max]:
+        index_max = i
+print(f'Разница = {list_fractions[index_max] - list_fractions[index_min]}')
