727fb00f7b9b0611ab501e9f1a962d805b6a7a4d
valentsk <skredovavalenti@mail.ru>
2022-12-03 14:07:17 +0300

    Add resolve 004

--- Homework003/Ex004.py
+++ Homework003/Ex004.py
@@ -5,3 +5,14 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+number = int(input('Введите число: '))
+temp = number
+res_str = ''
+while number // 2 > 0:
+    res_str += str(number % 2)
+    number = number // 2
+    if number == 1:
+        res_str += '1'
+
+print(f'dec {temp} = bin {res_str[::-1]}')
