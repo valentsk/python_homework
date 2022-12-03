f60fcd21a1f363da7053751b45813a65f6fa85e6
valentsk <skredovavalenti@mail.ru>
2022-12-03 15:43:59 +0300

    Resolve ex002

--- Homework004/Ex002.py
+++ Homework004/Ex002.py
@@ -1,1 +1,14 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+number = int(input('Введите число: '))
+
+list_result = []
+i = 2
+while number >= i:
+    if number % i == 0:
+        list_result.append(i)
+        number = number // i
+        i -= 1
+    i +=1
+print(list_result)
+
