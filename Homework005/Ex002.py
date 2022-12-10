e82c8bd0c87e9539229bb77020ad73cff4c935a9
valentsk <skredovavalenti@mail.ru>
2022-12-10 18:12:02 +0300

    Resolve ex02

--- Homework005/Ex002.py
+++ Homework005/Ex002.py
@@ -8,3 +8,54 @@
 # a) Добавьте игру против бота
 #
 # b) Подумайте как наделить бота ""интеллектом""
+
+import random
+
+first_step = random.randint(0,2)
+# 0 - PC, 1 - man
+candy_all = 2021
+max_take = 28
+print(f'Сейчас {candy_all} конфет')
+
+if first_step == 0:
+    print('Первый шаг за компьютером')
+    while candy_all > 0:
+        if candy_all > max_take:
+            pc_take = random.randint(1, 29)
+        else:
+            pc_take = random.randint(1, candy_all)
+        candy_all -= pc_take
+        print(f'Компьютер взял {pc_take} конфет. Осталось {candy_all} конфет')
+        if candy_all <= 0:
+            break
+        man_take = int(input('Бери конфеты (не больше 28): '))
+        if man_take <= max_take:
+            candy_all -= man_take
+            print(f'Человек взял {man_take} конфет. Осталось {candy_all} конфет')
+        else:
+            candy_all -= max_take
+            print(f'Еще раз, число конфет не больше 28. Тебе отдали максильное число конфет {max_take}. В следующий раз будь внимательнее. Осталось {candy_all} конфет')
+        if candy_all <= 0:
+            break
+else:
+    print('Первый шаг за человеком')
+    while candy_all > 0:
+        man_take = int(input('Бери конфеты (не больше 28): '))
+        if man_take <= max_take:
+            candy_all -= man_take
+            print(f'Человек взял {man_take} конфет. Осталось {candy_all} конфет')
+        else:
+            candy_all -= max_take
+            print(
+                f'Еще раз, число конфет не больше 28. Тебе отдали максильное число конфет {max_take}. В следующий раз будь внимательнее. Осталось {candy_all} конфет')
+        if candy_all <= 0:
+            break
+        if candy_all > max_take:
+            pc_take = random.randint(1, 29)
+        else:
+            pc_take = random.randint(1, candy_all)
+        candy_all -= pc_take
+        print(f'Компьютер взял {pc_take} конфет. Осталось {candy_all} конфет')
+        if candy_all <= 0:
+            break
+print('Победа!')
