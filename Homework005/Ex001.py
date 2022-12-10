a21ace6262e0e6486afdb7e8e7669b03cabb66dd
valentsk <skredovavalenti@mail.ru>
2022-12-10 17:07:55 +0300

    Resolve ex01

--- Homework005/Ex001.py
+++ Homework005/Ex001.py
@@ -1,7 +1,8 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
 str = input('Введите строку: ')
+# str = 'иииииииииииии абв авбддддддд вввввввввв ддадададаабвжж не удалитьабв'
 find_string = 'абв'
-list_str = list(str.split)
-result = map(lambda x: if find_string in x, list_str)
-print(result(list_str))
+list_str = list(str.split())
+list_result = list(map(lambda x: x.replace(x,'') if find_string in x else x, list_str))
+print(' '.join(list_result))
