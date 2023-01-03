3b8915511ae9e945e5bdf64a3af0664b749bdf19
valentsk <skredovavalenti@mail.ru>
2023-01-03 10:56:56 +0300

    Resolve ex08 and Happy New Year!

--- Homework007_008/controller.py
+++ Homework007_008/controller.py
@@ -9,7 +9,8 @@ def input_data():
     second_name = input("Введите отчество: ")
     phone_number = input("Введите телефон: ")
     address = input("Введите адрес: ")
-    return [last_name, first_name, second_name, phone_number, address]
+    cash = input("Введите ЗП: ")
+    return [last_name, first_name, second_name, phone_number, address, cash]
 
 
 def choice_separator():
@@ -23,7 +24,7 @@ def selection_menu():
     print("\nВыберите нужный пункт меню:\n\
         1 - импорт;\n\
         2 - экспорт;\n\
-        3 - поиск контакта;\n\
+        3 - меню поиска;\n\
         0 - выход из программы")
     menu_actions()
 
@@ -45,10 +46,10 @@ def menu_actions():
             if item:
                 print("ФАМИЛИЯ".center(15), "ИМЯ".center(15),
                       "ОТЧЕСТВО".center(15),
-                      "ТЕЛЕФОН".center(15), "АДРЕС".center(20))
-                print("-" * 90)
+                      "ТЕЛЕФОН".center(15), "АДРЕС".center(20), "ЗАРПЛАТА".center(20))
+                print("-" * 120)
                 print(item[0].ljust(15), item[1].ljust(15), item[2].ljust(15),
-                      item[3].ljust(15), item[4].ljust(20))
+                      item[3].ljust(15), item[4].ljust(20), item[5].ljust(20))
             else:
                 print("Данные не обнаружены")
             selection_menu()
--- Homework007_008/view.py
+++ Homework007_008/view.py
@@ -2,11 +2,11 @@
 def print_data(data):
     if len(data) > 0:
         print("ФАМИЛИЯ".center(15), "ИМЯ".center(15), "ОТЧЕСТВО".center(15),
-              "ТЕЛЕФОН".center(15), "АДРЕС".center(20))
+              "ТЕЛЕФОН".center(15), "АДРЕС".center(20), "ЗАРПЛАТА".center(20))
         print("-" * 90)
         for item in data:
             print(item[0].ljust(15), item[1].ljust(15), item[2].ljust(15),
-                  item[3].ljust(15), item[4].ljust(20))
+                  item[3].ljust(15), item[4].ljust(20), item[5].ljust(20))
     else:
         print("В справочнике нет данных!")
 
@@ -18,6 +18,7 @@ def selection_search_menu():
         3 - Отчество;\n\
         4 - Телефон;\n\
         5 - Адрес;\n\
+        6 - Нижней планке ЗП;\n\
         0 - выйти из программы")
     choice = input("Выберите параметр, по которому нужно произвести поиск: ")
     return choice
@@ -58,6 +59,11 @@ def search_data(data):
                     if search in item[4]:
                         print(item, sep=',')
                         # return item
+        case '6':
+            if len(data) > 0:
+                for item in data:
+                    if int(search) < int(item[5]):
+                        print(item, sep=',')
         case '0':
             exit(0)
         case _:
